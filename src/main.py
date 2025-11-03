3d7350e516e4898ee586d91b73b974df867af2dd
lin826 <liniju@bu.edu>
2025-11-02 22:29:35 -0500

    Add input data for preprocess

--- src/main.py
+++ src/main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from nano-gpt!")
+"""Main module to demonstrate encoding and decoding of strings using unique characters."""
+
+from pathlib import Path
+
+from utils.input_data import InputData
+from utils.input_parser import InputParser
 
 
 if __name__ == "__main__":
-    main()
+    print(f"Current folder: {Path.cwd()}\n============================")
+
+    # Parse the first text files in the default input directory
+    input_strings: list[str] = InputParser().parse()
+    sample_data = InputData(input_strings[0])
+
+    # Test encoding and decoding
+    ints = sample_data.encode("hii there")
+    print(f"Encoded integer list: {ints}")
+    decoded_result = sample_data.decode(ints)
+    print(f"Decoded string: {decoded_result}\n")
+
+    # Display tensor representation
+    tnesor = sample_data.get_tensor()
+    print(f"Tensor representation: {tnesor.shape} {tnesor.dtype}\n{tnesor[:100]}\n")
---
+++ src/utils/input_data.py
@@ -0,0 +1,44 @@
+"""A module for data handling and conversion between string and integer representations."""
+import torch
+
+
+class InputData:
+    '''A class for handling string data and converting it to and from integer representations.'''
+    def __init__(self, input_str: str):
+        self._input = input_str
+        self._update_mappings()
+        self._update_tensor()
+
+    def get_input(self) -> str:
+        '''Returns the original input string.'''
+        return self._input
+
+    def get_tensor(self) -> torch.Tensor:
+        '''Returns the tensor representation of the input string.'''
+        return self._tensor
+
+    def encode(self, s: str) -> list[int]:
+        '''Encodes a string into a list of integers based on character mappings.'''
+        return list(map(self._str_to_int, s))
+
+    def decode(self, int_list: list[int]) -> str:
+        '''Decodes a list of integers back into a string based on character mappings.'''
+        return ''.join(map(self._int_to_str, int_list))
+
+    def _get_unique_chars(self) -> str:
+        '''Returns a string of unique characters from the input text, sorted in order.'''
+        return ''.join(sorted(list(set(self._input))))
+
+    def _str_to_int(self, ch: str) -> int:
+        return self._str_to_int_mapping[ch]
+
+    def _int_to_str(self, i: int) -> str:
+        return self._int_to_str_mapping[i]
+
+    def _update_mappings(self) -> None:
+        unique_chars = self._get_unique_chars()
+        self._str_to_int_mapping = { ch:i for i,ch in enumerate(unique_chars) }
+        self._int_to_str_mapping = { i:ch for i,ch in enumerate(unique_chars) }
+
+    def _update_tensor(self) -> None:
+        self._tensor = torch.tensor(self.encode(self._input), dtype=torch.long)
